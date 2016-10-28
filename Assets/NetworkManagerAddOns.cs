1a2824ed924e0c4f6faf5ca024a90837ab02db62
ET-127 <eyitt1@gmail.com>
2016-10-28 20:47:33 +0100

    Understand IPs!
    It works!!

---
+++ Assets/NetworkManagerAddOns.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkManagerAddOns : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log (NetworkManager.singleton.networkAddress);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
