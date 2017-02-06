331730ea6d1a719c984b06f792acde0decf45963
ET-127 <eyitt1@gmail.com>
2017-02-06 16:57:39 +0000

    Small
    Minor

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 offset;
+	public float smoothTime;
+	Vector3 refVel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector3.SmoothDamp (transform.position,player.position + offset,ref refVel,smoothTime);
+		
+	}
+}
---
+++ Assets/PlayerRoll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoll : MonoBehaviour {
+
+	public Transform ball;
+	public float force;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponentInChildren<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		rb.AddTorque (transform.right * force * Input.GetAxisRaw("Vertical"));
+		rb.transform.Rotate (Vector3.up * Input.GetAxisRaw("Horizontal"),Space.World);
+		
+	}
+}
